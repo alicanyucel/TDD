f91bbeded62afe745b64f153a9018f518e4901eb
Ali Can Yücel <yucelalican@hotmail.com>
2026-03-24 14:58:53 +0300

    2 test basarılı gecti

--- TDD.Tests/ProductSpecification.cs
+++ TDD.Tests/ProductSpecification.cs
@@ -6,27 +6,70 @@ public class ProductSpecification
 {
     [Fact]
     //önce metod ismi  sonra ne olması gerektiği exception ne zaman
-    public async Task Create_Should_Be_Throw_Exception_If_Name_Less_Than_3_Characters ()
+    public async Task Create_Should_Be_Throw_Argument_Exception_If_Name_Less_Than_3_Characters ()
     {
-
+        /* Tdd cycle
+         1.Önce başarısz test yaz
+         testteki tüm clas v metotları olusuuyoruz
+        2)testi başarılı kıl
+        3)Refactor ile kodu iyileştiriyoruz
+         
+         */
         //Arrange öncelikle tanımalanan class
-        CreateProductCommand request = new ("La");
+        CreateProductCommand request = new ("La",1);
         CreateProductCommandHandler commnad = new();
 
         //Act işlem burada
-        var action = async ()=> await commnad.Handle(request, CancellationToken.None);
-
+        var action = async ()=> await commnad.Handle(request, default);
 
         //Assert işlem sonucnun testi işlem sonucu bu olmalı
-         await  action.Should().ThrowAsync<Exception>();
+        (await action.Should().ThrowAsync<ProductNameNotValidException>()).WithMessage("Name must be at least 3 characters long.");
+    }
+ 2   [Fact]
+    public async Task Create_Should_Be_Throw_Argumenet_Exception_If_Price_Equal_Or_Lower_Than_0()
+    {
+        //arrange 
+        CreateProductCommand request = new("Laptop", 0);
+        CreateProductCommandHandler commnad = new();
+
+        //act
+        var action=async()=>await commnad.Handle(request, default);
+
+
+        //Assert
+        await action.Should().ThrowAsync<ProductPriceNotValidException>();
+        
     }
 }
-public sealed record CreateProductCommand(string Name);
+public sealed record CreateProductCommand(string Name,decimal Price);
 public sealed class  CreateProductCommandHandler
 {
-    public async Task Handle(CreateProductCommand command,CancellationToken cancellationToken)
+    public async Task Handle(CreateProductCommand request,CancellationToken cancellationToken)
     {
-        throw new Exception("Name must be at least 3 characters long.");
+        if (request.Name.Length < 3)
+        {
+            throw new ProductNameNotValidException();
+        }
+
+        if (request.Price <= 0)
+        {
+            throw new ProductPriceNotValidException();
+        }
+
         await Task.CompletedTask;
     }
 }
+public sealed class ProductNameNotValidException : Exception
+{
+    public ProductNameNotValidException():base("Name must be at least 3 characters long.")
+    {
+        
+    }
+}
+    public sealed class ProductPriceNotValidException : Exception
+    {
+        public ProductPriceNotValidException():base("Price must be greater than 0")
+        {
+            
+        }
+    }
